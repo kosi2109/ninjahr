ffbbae78cd0021d881e34c0d9be9fc709c043392
Si Thu Htet <nolockfacebook55@gmail.com>
2022-04-21 14:32:12 +0630

    added postgres database configuration

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -17,14 +17,14 @@ class EmployeeSeeder extends Seeder
     public function run()
     {
         $role = new Role(["name" => "HR"]);
-        $view_role = new Role(["name" => "view_role"]);
-        $view_permission = new Role(["name" => "view_permission"]);
-        $create_role  = new Role(["name" => "create_role"]);
-        $create_permission  = new Role(["name" => "create_permission"]);
-        $edit_role  = new Role(["name" => "edit_role"]);
-        $edit_permission  = new Role(["name" => "edit_permission"]);
-        $delete_role  = new Role(["name" => "delete_role"]);
-        $delete_permission  = new Role(["name" => "edit_permission"]);
+        $view_role = new Permission(["name" => "view_role"]);
+        $view_permission = new Permission(["name" => "view_permission"]);
+        $create_role  = new Permission(["name" => "create_role"]);
+        $create_permission  = new Permission(["name" => "create_permission"]);
+        $edit_role  = new Permission(["name" => "edit_role"]);
+        $edit_permission  = new Permission(["name" => "edit_permission"]);
+        $delete_role  = new Permission(["name" => "delete_role"]);
+        $delete_permission  = new Permission(["name" => "edit_permission"]);
 
         $role->givePermissionTo([
             $view_role->id, 
@@ -36,18 +36,18 @@ class EmployeeSeeder extends Seeder
             $delete_role->id,
             $delete_permission->id,
         ]);
-        $user = new User([
-            'name' => "Si Thu Htet",
-            'email' => "sithuhtet.kosi21@gmail.com",
-            'password' => bcrypt('asdqwefr'), // password
-            'phone' => "09781903836",
-            'nrc_number' => "0/abc(N)09888",
-            'birthday' => "2000-09-21",
-            'gender' => "male",
-            'address' => "address",
-            'employee_id' => "e_0001",
-            'pin' => "111111",
-        ]);
-        $user->syncRoles($role->id);
+        // $user = new User([
+        //     'name' => "Si Thu Htet",
+        //     'email' => "sithuhtet.kosi21@gmail.com",
+        //     'password' => bcrypt('asdqwefr'), // password
+        //     'phone' => "09781903836",
+        //     'nrc_number' => "0/abc(N)09888",
+        //     'birthday' => "2000-09-21",
+        //     'gender' => "male",
+        //     'address' => "address",
+        //     'employee_id' => "e_0001",
+        //     'pin' => "111111",
+        // ]);
+        // $user->syncRoles($role->id);
     }
 }
